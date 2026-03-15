25db11764757e696d505827c77372bbdeca15ba5
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-03-15 15:19:19 +0530

    Uploaded Candies Question

---
+++ candies.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t-->0)
+		{
+		    boolean b = false;
+		    int n = sc.nextInt();
+		    
+		    int a[] = new int[2*n];
+		    
+		    for(int i=0;i<a.length;i++)
+		    {
+		        a[i] = sc.nextInt();
+		    }
+		    
+		    
+		    
+		    for(int i=0;i<a.length;i++)
+		    {
+		        int c=0;
+		        int x = a[i];
+		        
+		        for(int j=0;j<a.length;j++)
+		        {
+		            if(x == a[j])
+		            {
+		                c++;
+		            }
+		        }
+		        
+		        if(c>2)
+		        {
+		            b = true;
+		            break;
+		        }
+		    }
+		    if(b)
+		    {
+		        System.out.println("NO");
+		    }
+		    else{
+		        System.out.println("YES");
+		    }
+		}
+
+	}
+}
