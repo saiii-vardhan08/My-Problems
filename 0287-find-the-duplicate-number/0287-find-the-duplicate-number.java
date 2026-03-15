11d14fcba72a7cb681cb4819ae2ba6fe7eb0ad1c
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-03-15 23:24:35 +0530

    Add explanation for Floyd's algorithm in findDuplicate
    Added a comment explaining the use of Floyd's algorithm for finding duplicates.

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,6 +1,9 @@
 class Solution {
     public int findDuplicate(int[] a) {
 
+
+        // used floyd's algorithm of linked lists which uses slow pointer and fast pointer and when it meets, it breaks and make slow pointer to start and takes steps until slow is equal to fast then return slow
+        
         int slow = a[0];
         int fast = a[0];
 
