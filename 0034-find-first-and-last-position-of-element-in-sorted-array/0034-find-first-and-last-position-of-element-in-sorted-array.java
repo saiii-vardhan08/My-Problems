fdb6cb905ebd6ac2b4b57e95521ec01b12f2e22c
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-03-18 23:28:32 +0530

    Enhance code comments for searchRange method
    Added comments to clarify the use of binary search for finding first and last positions of an element in a sorted array.

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,4 +1,5 @@
 class Solution {
+    // first when ever there is a question to find in array the first though you should get is binary search because o(logn).
     public int[] searchRange(int[] a, int t) {
         
         int f = firstO(a,t);
@@ -7,6 +8,7 @@ class Solution {
         return new int[]{f,s};
         
         }
+    // for this question we have to find two occurances because if 8 8 8 is there we will get mid value as 8 and index also middle one but we want right side of middle one and left most of middle one so we go to right and towards left after getting middle value 
  public int firstO(int[] a, int t) {
 
     int l = 0;
