5ee07e2bfae1b44fa7d55b3b57cb0879fe3059ab
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-03-21 15:33:22 +0530

    Approach for number comparison in ascending order check
    Refactor number extraction logic to handle multi-digit numbers correctly.

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean areNumbersAscending(String s) {
-        
+        // I learned one new functions in characteres 1. character.isDigit() which is for checking a character is number or not, returns true or false 
+        // first i tried one approach which only checks each and every character if digit then store it in prev and compare this failed because if there is 12 which is two digit number it is checking like 1, 2.
+        // but this is not a case so i need to went for this approach when there are continuos numbers we need to form a number before storing or comparing.
         int sl = s.length();
         int prev = -1;
 
@@ -9,6 +11,9 @@ class Solution {
             if(Character.isDigit(s.charAt(i)))
             {
                int n = 0;
+
+                // converting two or more digit charactered number into integer number.
+                // charcter to number we have to remove '0'
                while(i<sl && Character.isDigit(s.charAt(i)))
                {
                     n = n*10 + s.charAt(i)-'0';
