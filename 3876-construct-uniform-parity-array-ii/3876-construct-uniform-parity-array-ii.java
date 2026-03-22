55d9ced9b27774159d9199374514a86b1017cd8b
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-03-22 11:40:55 +0530

    Enhance code comments for clarity on parity logic
    Add comments to explain the logic for uniform parity array.

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean uniformArray(int[] a) {
-
+// think logic first parity logic ev+ev = ev od+od = ev, ev+od = od, od+ev = od;
+// so we first want to find minimum element if the smallest is even then if there is odd one then by logic it is impossible to make it all even or odd 
+// so we find min and if there is odd element even if one then return false or return true
         int min = Integer.MAX_VALUE;
         int od=0;
         for(int i=0;i<a.length;i++)
