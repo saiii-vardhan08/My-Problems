bc58985e7d7983c64e4fe060448bb2aea3f74482
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-03-23 18:58:51 +0530

    Time: 0 ms (100%), Space: 43.5 MB (93.06%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,13 +1,11 @@
 class Solution {
     public void merge(int[] a1, int m, int[] a2, int n) {
-       
-         // I used two pointers
-        // since two arrays are given we go for parallel pointers pointing two arryas 
+        
         int a[] = new int[m+n];
         int i = 0;
         int j = 0;
         int k = 0;
-// this lopp for adding lower possible values in new array 
+
         while(i<m && j<n)
         {
             if(a1[i]<=a2[j])
@@ -19,13 +17,10 @@ class Solution {
                 a[k++] = a2[j++];
             }
         }
-
-        // this is for remaining elemensts in a1
         while(i<m)
         {
             a[k++] = a1[i++];
         }
-// this is for remaining elements in a2
         while(j<n)
         {
             a[k++] = a2[j++];
