8a352effe4411ec5b3a9037ed01878d5382d9c34
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-03-23 18:07:20 +0530

    Refine comments in merge function
    Updated comments for clarity and corrected typos.

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,11 +1,12 @@
 class Solution {
     public void merge(int[] a1, int m, int[] a2, int n) {
-        // we used two pointers
+        // I used two pointers
+        // since two arrays are given we go for parallel pointers pointing two arryas 
         int a[] = new int[m+n];
         int i = 0;
         int j = 0;
         int k = 0;
-
+// this lopp for adding lower possible values in new array 
         while(i<m && j<n)
         {
             if(a1[i]<=a2[j])
@@ -17,10 +18,12 @@ class Solution {
                 a[k++] = a2[j++];
             }
         }
+// this is for remaining elemensts in a1
         while(i<m)
         {
             a[k++] = a1[i++];
         }
+// this is for remaining elements in a2
         while(j<n)
         {
             a[k++] = a2[j++];
