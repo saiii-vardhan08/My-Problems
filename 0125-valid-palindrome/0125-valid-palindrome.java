0a8a5824807ca89175408d539a40d35db201b2bf
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-03-23 18:12:24 +0530

    Approach for Ispalindrome
    Added comments to explain regex usage and two-pointer approach for palindrome check.

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
+        // this is regex which replaces everything with empty string 
+        //like ^ this is not operator which tells not letters and not numbers
+        // anything except these will be replaced 
+
+        // I used two pointers 
+        // since palidrome check i go for converging pointers
         String s1 = s.replaceAll("[^a-zA-Z0-9]","");
         s = s1.toLowerCase();
         int i=0;
