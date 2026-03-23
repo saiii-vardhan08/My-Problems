36dfa2a06e8de3dade905d49b41b7bc90b8edefe
saivardhan <saivardhan200@outlook.com>
2026-03-23 18:40:06 +0530

    Using inbuilt function for approach

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,7 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        // java has inbuilt function of indexOf which return the first occurance a string or character or string in particular string 
+       
+         // java has inbuilt function of indexOf which return the first occurance a string or character or string in particular string 
         if(haystack.indexOf(needle)!=-1)
         {
             return haystack.indexOf(needle);
