339e66a7d450beb929a387a79a1a180d85ecdbd9
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-03-23 19:02:15 +0530

    Refactor merge method in Solution class
    Refactor merge method to improve clarity and functionality.

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,6 +1,9 @@
 class Solution {
     public void merge(int[] a1, int m, int[] a2, int n) {
-        
+
+
+        // Used two pointers Approach 
+        //since two arrays parallel pointers approach 
         int a[] = new int[m+n];
         int i = 0;
         int j = 0;
@@ -17,6 +20,8 @@ class Solution {
                 a[k++] = a2[j++];
             }
         }
+
+        // to add remaining elements in new arryay
         while(i<m)
         {
             a[k++] = a1[i++];
