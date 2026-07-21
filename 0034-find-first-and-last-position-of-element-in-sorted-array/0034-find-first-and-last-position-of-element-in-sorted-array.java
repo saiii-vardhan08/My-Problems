04f8bf4c91ceeba848dc2dbe37e481e23c0e355c
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-07-21 23:14:02 +0530

    Time: 0 ms (100%), Space: 48.1 MB (51.64%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,66 +1,53 @@
 class Solution {
     public int[] searchRange(int[] a, int t) {
         
-        int f = firstO(a,t);
-        int s = secondO(a,t);
+        int ans[] = new int[]{-1,-1};
 
-        return new int[]{f,s};
-        
-        }
-    // when ever asked in qns to find in arrays concept just go for binary search technique because its tc is o(logn) so here if 8 8 8 occurs still thre is fO and sO so we want to go for right and left side of the mifddle element
- public int firstO(int[] a, int t) {
+        int l=0;
+        int h = a.length-1;
 
-    int l = 0;
-    int r = a.length-1;
-    int ans = -1;
-    while(l<=r)
-    {
-        int mid = (l+r)/2;
-        
-
-        if(a[mid]==t)
+        while(l<=h)
         {
-            ans = mid;
-            r = mid - 1;
+            int mid = l+(h-l)/2;
+
+            if(a[mid]==t)
+            {
+                ans[0] = mid;
+                h = mid-1;
+            }
+            else if(a[mid]<t)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                h = mid-1;
+            }
+            
         }
-        else if(a[mid]<t)
+
+        l=0;
+        h = a.length-1;
+
+        while(l<=h)
         {
-            l = mid + 1;
+            int mid = l+(h-l)/2;
+
+            if(a[mid]==t)
+            {
+                ans[1] = mid;
+                l=mid+1;
+            }
+            else if(a[mid]<t)
+            {
+                l = mid+1;
+            }
+            else
+            {
+                h=mid-1;
+            }
         }
-        else{
-            r = mid - 1;
-        }
-         
+
+        return ans;
     }
-    return ans;
- }
-
- public int secondO(int[] a, int t) {
-
-    int l = 0;
-    int r = a.length-1;
-    int ans = -1;
-    while(l<=r)
-    {
-        int mid = (l+r)/2;
-
-        if(a[mid]==t)
-        {
-            ans = mid;
-            l = mid + 1;
-        }
-        else if(a[mid]<t)
-        {
-            l = mid + 1;
-        }
-        else{
-            r = mid - 1;
-        }
-    }
-    return ans;
- }
 }
-
-
-
-
