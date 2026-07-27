475cba31bce4443913282be661307abe2e60e5bc
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-07-28 00:20:45 +0530

    Time: 21 ms (8.2%), Space: 42.9 MB (8.37%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        int ans=0;
+
+         for(long i=1;i*i<=x;i++)
+         {
+            ans = (int)i;
+         }
+
+         return ans;
+    }
+}
