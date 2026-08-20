de41afee8b46fc2620a01c849a05182c9a4633d9
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-08-21 01:06:03 +0530

    Time: 4 ms (83.07%), Space: 44.3 MB (82.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
         int a[] = new int[26];
 
         for(int i=0;i<s.length();i++)
