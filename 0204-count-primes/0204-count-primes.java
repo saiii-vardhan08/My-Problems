0cc332729029b8f2c99932063cd8567e97129d1b
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-08-26 00:28:19 +0530

    Time: 840 ms (5.03%), Space: 260.3 MB (5.43%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,27 +1,12 @@
 class Solution {
     public int countPrimes(int n) {
-        
-        if(n==0) return 0;
-        if(n==1) return 0;
-        int b[] =  sieve(n);
-        int c=0;
-        for(int i=2;i<n;i++)
-        {
-            if(b[i]==1)
-            {
-                c++;
-            }
-        }
-        return c;
-    }
-    public int[] sieve(int n)
-    {
-        int a[] = new int[n];
+        if(n<=1) return 0;
 
+        int a[] = new int[n];
         Arrays.fill(a,1);
-        
-        a[0] = 0;
-        a[1] = 0;
+        a[0]=0;
+        a[1]=0;
+
         for(int i=2;i*i<n;i++)
         {
             if(a[i]==1)
@@ -33,6 +18,15 @@ class Solution {
             }
         }
 
-        return a;
+int c=0;
+        for(int i=2;i<n;i++)
+        {
+            if(a[i]==1)
+            {
+                c++;
+            }
+
+        }
+        return c;
     }
 }
